f2dbefe4c4cb313d46a79ae47e87d2f4f19d1a15
Jerome Pineda <jeromepineda79@gmail.com>
2024-02-23 10:32:08 -0800

    revert changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack: (config) => {
-      config.externals = [...config.externals, { canvas: "canvas" }]; // required to make Konva & react-konva work
-      return config;
-    },
-    experimental: {
-      esmExternals: "loose", // required to make Konva & react-konva work
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
