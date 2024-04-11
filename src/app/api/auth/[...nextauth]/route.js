b6ee5ce6650e1fbfdd6cb856d2a41ca31a7931b1
tammyxa <icecoldxD123@gmail.com>
2024-04-11 16:32:25 -0700

    fix: deployment failed; export valid next.js api route function

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,20 +1,20 @@
-
 // Import necessary modules and dependencies
-import NextAuth from "next-auth"; //Oauth
-import { PrismaAdapter } from "@next-auth/prisma-adapter"; //Email and Password
-import { PrismaClient } from "@prisma/client"; //Email and Passowrd
-import { options } from "./options"; // Import authentication options OAuth
+import NextAuth from "next-auth"; 
+import { PrismaAdapter } from "@next-auth/prisma-adapter"; 
+import { PrismaClient } from "@prisma/client"; 
+import { options } from "./options"; // Import authentication options
 
 // Create a new instance of PrismaClient for database operations
 const prisma = new PrismaClient();
 
 // Configure the adapter to use Prisma for email/password authentication
-const authOptions = {
-  adapter: PrismaAdapter(prisma),
-};
+const adapter = PrismaAdapter(prisma);
 
-// OAuth authentication handler using options defined in options.js
-const handler = NextAuth(options);
+// Initialize NextAuth with the provided options and adapter
+const authHandler = NextAuth({
+  ...options,
+  adapter,
+});
 
-// Export the OAuth authentication handler
-export default handler;
+// Export the authentication handler function
+export default authHandler;
