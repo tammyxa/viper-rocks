8a2e5ddc91a420c4b6ba5b80b082ab25c381e419
tammyxa <icecoldxD123@gmail.com>
2024-04-11 15:53:27 -0700

    chore: tailwind config

---
+++ src/components/BackToTop/BackToTop.ts
@@ -0,0 +1,42 @@
+export const BackToTop = (backToTop: any) => {
+  const backToTopBtn = backToTop.querySelector("button");
+  const threshold = parseFloat(backToTop.dataset.threshold) || 300;
+  const scrollTo = parseFloat(backToTop.dataset.scrollto) || 0;
+  const alwaysVisible = backToTop.dataset.alwaysvisible;
+
+  const debounce = (func: any, wait = 0) => {
+    let timeoutId: any;
+    return (...args: any) => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        func(...args);
+      }, wait);
+    };
+  };
+
+  const onScroll = () => {
+    if (!alwaysVisible) {
+      let showBackToTop = window.scrollY > threshold;
+      showBackToTop
+        ? (backToTopBtn.style.display = "block")
+        : (backToTopBtn.style.display = "none");
+    }
+  };
+
+  const debouncedOnScroll = debounce(onScroll, 500);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: scrollTo, behavior: "smooth" });
+  };
+
+  if (!alwaysVisible) {
+    backToTopBtn.style.display = "none";
+    window.addEventListener("scroll", debouncedOnScroll, { passive: true });
+    backToTopBtn.addEventListener("click", scrollToTop);
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const backToTopElements = document.querySelectorAll(".BackToTop");
+  backToTopElements.forEach(BackToTop);
+});
