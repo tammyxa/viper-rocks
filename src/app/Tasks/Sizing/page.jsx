88031e2797330bdb7117a412ae2dfbb5dfee44c2
Jerome Pineda <jeromepineda79@gmail.com>
2024-04-29 10:11:35 -0700

    Update page.jsx

--- src/app/Tasks/Sizing/page.jsx
+++ src/app/Tasks/Sizing/page.jsx
@@ -3,22 +3,12 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 import dynamic from 'next/dynamic';
-import { signIn, useSession } from "next-auth/react";
 
 const DisplayQuadrant = dynamic(() => import('../../(components)/Sizing/DisplayQuadrant/canvas'), {
   ssr: false,
 });
 
 const SizingPage = () => {
-  const { data: session, status } = useSession();
-
-  useEffect(() => {
-    // Redirect if not authenticated
-    if (status === "unauthenticated") {
-      signIn('auth0', { callbackUrl: '/Tasks/Sizing' });
-    }
-  }, [status]);
-
   const [quadrants, setQuadrants] = useState([]);
   
   // Retrieve the currentIndex from localStorage or default to 0 if not found
@@ -70,8 +60,7 @@ const SizingPage = () => {
     });
     if (response.ok) {
       console.log('Submission successful');
-      localStorage.removeItem('savedLabels'); // Clear saved labels from localStorage on submit
-      setLabels([]);
+      setLabels([]);  // Clear labels on successful submission
       handleNextQuadrant();
     } else {
       console.error('Submission failed');
